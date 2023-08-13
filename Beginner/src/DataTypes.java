886163059fad015accc64c21ecf528ab50709499
shraddhabhat06 <shraddhabhat06@gmail.com>
2023-08-13 12:04:02 +0530

    update

---
+++ Beginner/src/DataTypes.java
@@ -0,0 +1,23 @@
+public class DataTypes {
+  public static void main(String[] args){
+
+    int a= 10;
+    double b =2.56789 ;
+    char c='a';
+    String d="Hello";
+    long e= 117;
+    float f=10.5f;
+    boolean g= true;
+    short h = 10;
+
+    System.out.println("Data Types");
+        System.out.printf("a = %d%n", a);
+        System.out.printf("b = %.5f%n", b);
+        System.out.printf("c = %c%n", c);
+        System.out.printf("d = %s%n", d);
+        System.out.printf("e = %d%n", e);
+        System.out.printf("f = %.2f%n", f);
+        System.out.printf("g = %b%n", g);
+        System.out.printf("h = %d%n", h);
+  }
+}
